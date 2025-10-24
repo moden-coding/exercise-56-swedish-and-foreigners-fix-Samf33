c35159ef3cea29faa8f0a359f6931d27172a8484
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-10-24 09:53:40 -0400

    cscdeioerfijo

--- src/swedish_and_foreigners.py
+++ src/swedish_and_foreigners.py
@@ -3,7 +3,16 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    pass
+    df1 = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = df1["Akaa": "Äänekoski"]
+
+    print(df.loc["Eckerö"])
+    mask = df["Share of Swedish-speakers of the population, %"]> 5
+    mask2 =  df["Share of foreign citizens of the population, %"] > 5
+    df_masked = df[mask]
+    # print(df_masked[mask2].head()[["Population","Share of foreign citizens of the population, %", "Share of Swedish-speakers of the population, %"]])
+
+    return df_masked[mask2][["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]]
 
 
 def main():
